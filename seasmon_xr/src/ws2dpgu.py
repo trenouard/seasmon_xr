98a0682c37856fcb1c29c8bca6ef8929a01ddc57
Valentin Pesendorfer <valentin.pesendorfer@wfp.org>
2021-07-16 10:07:17 +0200

    add check for s==0.0

--- seasmon_xr/src/ws2dpgu.py
+++ seasmon_xr/src/ws2dpgu.py
@@ -16,50 +16,54 @@ def ws2dpgu(y, lmda, nodata, p, out):
         Smoothed time-series array z
     """
 
-    w = numpy.zeros(y.shape, dtype=float64)
-    m = y.shape[0]
-    n = 0
+    if lmda != 0.0:
 
-    for ii in range(m):
-        if y[ii] == nodata:
-            w[ii] = 0
+        w = numpy.zeros(y.shape, dtype=float64)
+        m = y.shape[0]
+        n = 0
+
+        for ii in range(m):
+            if y[ii] == nodata:
+                w[ii] = 0
+            else:
+                n += 1
+                w[ii] = 1
+
+        if n > 1:
+            p1 = 1-p
+            z = numpy.zeros(m)
+            znew = numpy.zeros(m)
+            wa = numpy.zeros(m)
+            ww = numpy.zeros(m)
+
+            # Calculate weights
+
+            for _ in range(10):
+                for j in range(m):
+                    y_tmp = y[j]
+                    z_tmp = z[j]
+
+                    if y_tmp > z_tmp:
+                        wa[j] = p
+                    else:
+                        wa[j] = p1
+                    ww[j] = w[j] * wa[j]
+
+                znew[:] = ws2d(y, lmda, ww)
+                z_tmp = 0.0
+                j = 0
+                for j in range(m):
+                    z_tmp += abs(znew[j] - z[j])
+
+                if z_tmp == 0.0:
+                    break
+
+                z[:] = znew[:]
+
+            z = ws2d(y, lmda, ww)
+            numpy.round_(z, 0, out)
+
         else:
-            n += 1
-            w[ii] = 1
-
-    if n > 1:
-        p1 = 1-p
-        z = numpy.zeros(m)
-        znew = numpy.zeros(m)
-        wa = numpy.zeros(m)
-        ww = numpy.zeros(m)
-
-        # Calculate weights
-
-        for _ in range(10):
-            for j in range(m):
-                y_tmp = y[j]
-                z_tmp = z[j]
-
-                if y_tmp > z_tmp:
-                    wa[j] = p
-                else:
-                    wa[j] = p1
-                ww[j] = w[j] * wa[j]
-
-            znew[:] = ws2d(y, lmda, ww)
-            z_tmp = 0.0
-            j = 0
-            for j in range(m):
-                z_tmp += abs(znew[j] - z[j])
-
-            if z_tmp == 0.0:
-                break
-
-            z[:] = znew[:]
-
-        z = ws2d(y, lmda, ww)
-        numpy.round_(z, 0, out)
-
+            out[:] = y[:]
     else:
-        out[:] = y[:]
+            out[:] = y[:]
