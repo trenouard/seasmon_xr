6cae64b22c65de71503fed0f10c1029e01a2ea24
Valentin Pesendorfer <valentin.pesendorfer@wfp.org>
2021-08-16 17:49:29 +0200

    implement cal_ix; fix issue with 0s in gammafit

--- seasmon_xr/src/spi.py
+++ seasmon_xr/src/spi.py
@@ -107,7 +107,7 @@ def brentq(xa, xb, s):
 @numba.njit
 def gammafit(x):
     """Calculate gamma distribution parameters for timeseries
-    
+
     Adapted from:
     https://github.com/scipy/scipy/blob/f2ef65dc7f00672496d7de6154744fee55ef95e9/scipy/stats/_continuous_distns.py#L2554
     Copyright (c) 2001-2002 Enthought, Inc.  2003-2019, SciPy Developers.
@@ -124,9 +124,12 @@ def gammafit(x):
             logs+= log(xx)
             n +=1
 
+    if n == 0:
+        return (0, 0)
+
     xtsbar = xts / n
     s = log(xtsbar) - (logs / n)
-    
+
     if s == 0:
         return (0, 0)
 
@@ -136,23 +139,20 @@ def gammafit(x):
     a = brentq(xa, xb, s)
     if a == 0:
         return (0, 0)
-              
+
     b = xtsbar / a
-    
+
     return (a, b)
 
 @numba.njit
-def spifun(x, a=None, b=None, cal_start=None, cal_stop=None):
+def spifun(x, a=None, b=None, cal_ix=None):
     """Calculate SPI with gamma distribution for 3d array
     """
     y = np.full_like(x, -9999)
     r,c,t = x.shape
-    
-    if cal_start is None:
-        cal_start = 0
-    
-    if cal_stop is None:
-        cal_stop = t
+
+    if cal_ix is None:
+        cal_ix = np.arange(0, t)
 
     for ri in range(r):
         for ci in range(c):
@@ -173,10 +173,10 @@ def spifun(x, a=None, b=None, cal_start=None, cal_stop=None):
                 continue
 
             if a is None or b is None:
-                alpha, beta = gammafit(xt[cal_start:cal_stop])
+                alpha, beta = gammafit(xt[cal_ix])
             else:
                 alpha, beta = (a, b)
-            
+
             if alpha == 0 or beta == 0:
                 y[ri, ci, :] = -9999
                 continue
